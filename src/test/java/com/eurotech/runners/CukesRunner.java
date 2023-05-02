fb6bdef4d7526f66cf27872544470f2c0b45a1ce
Ahmet <ahmetyilmaznnb2005@gmail.com>
2023-05-02 17:42:05 +0200

    Dashboard

--- src/test/java/com/eurotech/runners/CukesRunner.java
+++ src/test/java/com/eurotech/runners/CukesRunner.java
@@ -12,9 +12,10 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/eurotech/step_definitions",
         dryRun = false,
-        tags = "@smoke"
+        tags = "@wip"
 
 
 )
 public class CukesRunner {
+
 }
